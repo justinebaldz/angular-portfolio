012bea94df06c76964f849e6a8376858131ba190
Justine Baldonado <justeinbaldonado@gmail.com>
2022-07-24 23:03:07 +0800

    Fix assets src for deployment

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -20,32 +20,32 @@ export class ProjectsComponent implements OnInit {
         id: '0',
         title: 'JUANTAP WEBSITE',
         description: 'This is Case study project created using HTML, CSS, Javascript and PHP.',
-        path: '/assets/img/projects/juantap.png'
+        path: 'assets/img/projects/juantap.png'
       },
       {
         id: '1',
         title: 'CUBING.IO CUBESHOP',
         description: 'A school project created using HTML and CSS',
-        path: '/assets/img/projects/cubeshop.png'
+        path: 'assets/img/projects/cubeshop.png'
       },
       {
         id: '2',
         title: 'SPEED CUBING TIMER',
         description: "A timing program designed for Rubik's Cube Speedsolves.",
-        path: '/assets/img/projects/timer.png'
+        path: 'assets/img/projects/timer.png'
       },
       {
         id: '3',
         title: 'JUANTAP APP UI',
         description: 'A case study project created using Java and PHP for Juantap users.',
-        path: '/assets/img/projects/mob.png'
+        path: 'assets/img/projects/mob.png'
       },
     ];
 
     this.imgSrc = 'assets/img/projects/juantap.png';
     this.projectTitle = 'JUANTAP WEBSITE';
     this.projectDescription = 'This is Case study project created using HTML, CSS, Javascript and PHP.';
-    this.projectPath = '/assets/img/projects/juantap.png';
+    this.projectPath = 'assets/img/projects/juantap.png';
   }
 
   ngOnInit(): void {}
