43f98807f8acec405ddf00e491ba341ab5e08eba
Justine Baldonado <justeinbaldonado@gmail.com>
2022-07-25 14:55:00 +0800

    fix ui
    - add material module
    - add font awesome module
    - create pdf viewer for CV
    - change icon for project section next and previous button
    - fix  alignment for footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LinkListenerService } from 'src/app/link-listener.service';
+import { CvHomeComponent } from 'src/app/components/cv-home/cv-home.component';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,7 +14,8 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private linkService: LinkListenerService,
-    private router: Router
+    private router: Router,
+    public dialog: MatDialog
   ) {
     this.navItems = this.linkService.activeNavItemSubject$;
 
@@ -27,6 +30,17 @@ export class HomeComponent implements OnInit {
     // console.log(this.linkService.navItems[this.router.url]);
 
     this.linkService.setNavItem(this.linkService.navItems[this.router.url]);
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(CvHomeComponent, {
+      height: 'auto',
+      width: '50%',
+    });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log(`Dialog result: ${result}`);
+    // });
   }
   ngOnInit(): void {}
 }
